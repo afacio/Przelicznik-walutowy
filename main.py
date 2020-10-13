384ea3888032ba9f6c5a2143fff77d0ca5f94f3f
afacio <afacio@users.noreply.github.com>
2020-10-13 13:35:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,22 +15,20 @@ class Waluta():
         print("\n=================================")
         print("nazwa waluty: ", self.nazwa_waluty, "\nkod_waluty: ", self.kod_waluty, "\nprzelicznik: ", self.przelicznik, "\nkurs_sredni: ", self.kurs_sredni)
 
-class ZbiorWalut():
+class Plik():
     def __init__(self):
-        self.zbior = list()
-
+        self.url = 'https://www.nbp.pl/kursy/xml/lasta.xml'
     def download_file(self):
         try:
-            url = 'https://www.nbp.pl/kursy/xml/lasta.xml'
-            r = requests.get(url, allow_redirects=True)
+            r = requests.get(self.url, allow_redirects=True)
             open('lasta.xml', 'wb').write(r.content)
         except requests.ConnectionError:
             print("brak polaczenia z internetem")
             exit(1)
 
     def czytajPlik(self, zbior):
-        file = r"lasta.xml"
 
+        file = r"lasta.xml"
         tree = ET.parse(file)
         root = tree.getroot()
 
@@ -44,40 +42,46 @@ class ZbiorWalut():
             waluta.kurs_sredni = string_to_float(kurs_sredni)
             zbior.append(waluta)
 
-    def wybor(self):
-        while (True):
-            var1 = input("\nWpisz kod waluty jakiej chcesz użyć: ")
-            var2 = 0
+class ZbiorWalut():
+    def __init__(self):
+        self.zbior = list()
 
-            for waluta in self.zbior:
-                if var1 == waluta.kod_waluty:
-                    print("Wybrales walute: ", waluta.nazwa_waluty)
-                    return waluta
-                else:
-                    var2 += 1
-                if var2 == len(self.zbior):
-                    print("brak podanego kodu waluty, sprobuj ponownie", end='')
-                    continue
-    def wyswietl_kod_waluty(self, zbior):
-        for waluta in zbior:
+    def wyswietl_kod_waluty(self):
+        for waluta in self.zbior:
             waluta.print_currency_code()
-    def wyswietl_wszystko(self, zbior):
-        for waluta in zbior:
+
+    def wyswietl_wszystko(self):
+        for waluta in self.zbior:
             waluta.print_all()
 
-def przelicznik(var1, var2):
-    proba = True
-    while(proba):
-        suma_string = input("\nPodaj sumę pieniędzy ktora chcesz przeliczyć z waluty {0} na {1}: ".format(var1.kod_waluty, var2.kod_waluty))
-        try:
-            suma_float = float(suma_string)
-            print(suma_float)
-            PLN = suma_float * var1.przelicznik * var1.kurs_sredni
-            suma_koncowa = PLN * var2.przelicznik / var2.kurs_sredni
-            print(round(suma_koncowa, 4))
-            proba = False
-        except ValueError:
-            print("Wprowadz wartosc typu float")
+    def wybor(self):
+            while (True):
+                var1 = input("\nWpisz kod waluty jakiej chcesz użyć: ")
+                var2 = 0
+
+                for waluta in self.zbior:
+                    if var1 == waluta.kod_waluty:
+                        print("Wybrales walute: ", waluta.nazwa_waluty)
+                        return waluta
+                    else:
+                        var2 += 1
+                    if var2 == len(self.zbior):
+                        print("brak podanego kodu waluty, sprobuj ponownie", end='')
+                        continue
+
+    def przelicznik(self, var1, var2):
+        proba = True
+        while(proba):
+            suma_string = input("\nPodaj sumę pieniędzy ktora chcesz przeliczyć z waluty {0} na {1}: ".format(var1.kod_waluty, var2.kod_waluty))
+            try:
+                suma_float = float(suma_string)
+                print(suma_float)
+                PLN = suma_float * var1.przelicznik * var1.kurs_sredni
+                suma_koncowa = PLN * var2.przelicznik / var2.kurs_sredni
+                print(round(suma_koncowa, 4))
+                proba = False
+            except ValueError:
+                print("Wprowadz wartosc typu float")
 
 def string_to_float(string):
     return round(float(zmiana_przecinka(string)),4)
@@ -97,21 +101,24 @@ def zmiana_przecinka(str):
     return string
 
 def main():
-    zbior = ZbiorWalut()
-    zbior.download_file()
-    zbior.czytajPlik(zbior.zbior)
-
     program = True
+
+    plik = Plik()
+    plik.download_file()
+
+    zbior_walut = ZbiorWalut()
+    plik.czytajPlik(zbior_walut.zbior)
+
     MENU = "\nWybierz akcje:\n1: Podglad kursu walut\n2: Wymiana walutowa\n3: Koniec programu\n"
     while (program):
         wybor = input(MENU)
         if wybor == '1':
-            zbior.wyswietl_wszystko(zbior.zbior)
+            zbior_walut.wyswietl_wszystko()
         elif wybor == '2':
-            zbior.wyswietl_kod_waluty(zbior.zbior)
-            waluta1 = zbior.wybor()
-            waluta2 = zbior.wybor()
-            przelicznik(waluta1, waluta2)
+            zbior_walut.wyswietl_kod_waluty()
+            waluta1 = zbior_walut.wybor()
+            waluta2 = zbior_walut.wybor()
+            zbior_walut.przelicznik(waluta1, waluta2)
         elif wybor == '3':
             program = False
         else:
